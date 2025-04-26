0a002ea35572bf21d3bb6ba1d3555b7d08fa9c77
Hitjunior001 <juninhorf8@Hotmail.com>
2025-04-25 22:40:02 -0300

    deplloy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/my-kanban/', // 👈 Isso aqui é ESSENCIAL para funcionar no GitHub Pages
-
   plugins: [
     tailwindcss(),
   ],
